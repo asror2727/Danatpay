02531a3b8611510066aa3e36c7b666e6acda38d1
asror2727 <asrorake001@gmail.com>
2026-07-19 01:02:44 +0500

    Add files via upload

---
+++ themes.js
@@ -0,0 +1,7 @@
+module.exports = {
+  dark: { name: 'Dark', price: 0 },
+  neon: { name: 'Neon', price: 30000 },
+  glass: { name: 'Glass', price: 25000 },
+  gold: { name: 'Gold', price: 50000 },
+  anime: { name: 'Anime', price: 35000 }
+};
